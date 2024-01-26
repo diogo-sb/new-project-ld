bb9bfc3a1cd4817c97ea7e90504a36aae8435e7f
Diogo Brito <66088183+diogo-sb@users.noreply.github.com>
2024-01-26 00:58:46 -0300

    Update connect_sql_python.py

--- connect_sql_python.py
+++ connect_sql_python.py
@@ -6,21 +6,13 @@ db = mysql.connector.connect(
     user="root",
     passwd="",
     database="projeto"
-    #uth_plugin='mysql_native_password'
 )
 
 #select no banco
 cursor = db.cursor()
 cursor.execute("SELECT * FROM POSTS")
-<<<<<<< HEAD
 result = cursor.fetchall()
+
 #vendo os resultados
 for x in result:
     print(x)
-=======
-
-myresult = cursor.fetchall()
-
-for x in myresult:
-  print(x)
->>>>>>> 6ccfa043702ce8d7b531cc6087684cbc8665c7f1
