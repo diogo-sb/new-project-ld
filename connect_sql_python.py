6ccfa043702ce8d7b531cc6087684cbc8665c7f1
Diogo Brito <66088183+diogo-sb@users.noreply.github.com>
2024-01-26 00:52:48 -0300

    Update connect_sql_python.py

--- connect_sql_python.py
+++ connect_sql_python.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="project123",
+    passwd="",
     database="projeto"
     #uth_plugin='mysql_native_password'
 )
