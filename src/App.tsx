321032fecb99e6ab87c7a859594c37e29f2db61d
Lucas Dantas <dants.dev@gmail.com>
2024-01-27 22:06:44 -0300

    feat: implementado react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
-import { ThemeProvider } from "@/components/theme-provider"
-import { Button } from './components/ui/button'
+import "./App.css";
+import Home from "./pages/home/home"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggler";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Button>Hello</Button>
+      <ModeToggle></ModeToggle>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import { Textarea } from "@/components/ui/textarea"
+
+export default function Home() {
+    return(
+        <>
+        <main>
+            <Textarea placeholder="Digite aqui"></Textarea>
+        </main>
+        </>
+    )
+}
