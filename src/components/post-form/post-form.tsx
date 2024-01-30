c54d6f479c8678f4491334eb1afbe6775766acd0
Lucas Dantas <dants.dev@gmail.com>
2024-01-29 22:50:34 -0300

    feat: implementado form Closes #16

--- src/components/post-form/post-form.tsx
+++ src/components/post-form/post-form.tsx
@@ -1,20 +1,59 @@
 import { Textarea } from '@/components/ui/textarea'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { toast } from 'sonner'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
 import { Button } from '../ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+
+const formSchema = z.object({
+  posts: z.string().min(10, { message: 'Digite no minimo 10 palavras' }).max(250),
+  user: z.string(),
+  dat: z.number(),
+})
 
 export default function PostForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      user: 'Usuario1',
+      posts: '',
+      dat: new Date().getTime(),
+    },
+  })
+
   return (
-    <div className='flex flex-col items-end'>
-      <Textarea placeholder='No que esta pensando?' />
-      <Button className='mt-3'
-        onClick={() => {
-          toast('Enviado!', {
-            description: 'Seu post foi publicado',
-          })
-        }}
-      >
-        Enviar
-      </Button>
-    </div>
+    <Form {...form}>
+      <form className='' onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='posts'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>POST</FormLabel>
+              <FormControl>
+                <Textarea placeholder='No que você esta pensando?' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='mt-3'>
+          Enviar
+        </Button>
+      </form>
+    </Form>
   )
+}
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  toast('Post publicado com sucesso')
+  console.log(values)
 }
